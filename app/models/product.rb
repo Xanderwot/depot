fc3e2db6e6bdfe40c855741d7eb3ebf46b01f81f
Xander <xander@xander-N150P-N210P-N220P.(none)>
2012-03-21 23:30:18 +0300

    some fixes in validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,13 @@ class Product < ActiveRecord::Base
 	validates :title, :presence => true,
 	                  :uniqueness => true,
 	                  :length => { :within => (4..50),
-	                  			   :message => 'Title must be betwin 4 and 50 characters' }             
+	                  			   :message => 'must be betwin 4 and 50 characters' }             
 
 	validates :description, :presence => true
 
 	validates :image_url, :presence => true,
 	 					  :format => { :with => (%r{\.(gif|jpg|png)$}i),
-									   :message => 'Must be a .gif, .jpg or .png image'}	
+									   :message => 'must be a .gif, .jpg or .png image'}	
 
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 
