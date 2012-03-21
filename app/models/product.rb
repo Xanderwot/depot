8e7b95fa4066f583bb30fa9113ecad725cc15337
Xander <xander@xander-N150P-N210P-N220P.(none)>
2012-03-21 23:24:27 +0300

    task b. Add validates to app

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ActiveRecord::Base
+
+	validates :title, :presence => true,
+	                  :uniqueness => true,
+	                  :length => { :within => (4..50),
+	                  			   :message => 'Title must be betwin 4 and 50 characters' }             
+
+	validates :description, :presence => true
+
+	validates :image_url, :presence => true,
+	 					  :format => { :with => (%r{\.(gif|jpg|png)$}i),
+									   :message => 'Must be a .gif, .jpg or .png image'}	
+
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+
 end
