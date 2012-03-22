73f96fc76053f91bd503a6ea88c72ecece84f18f
Xander <xander@xander-N150P-N210P-N220P.(none)>
2012-03-22 17:58:42 +0300

    Task D. Creating Cart & LineItem models and controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ActiveRecord::Base
 
+	default_scope :order => 'title'
+
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :presence => true,
 	                  :uniqueness => true,
 	                  :length => { :within => (4..50),
@@ -12,5 +18,17 @@ class Product < ActiveRecord::Base
 									   :message => 'must be a .gif, .jpg or .png image'}	
 
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+
+	private
+	
+		def ensure_not_referenced_by_any_line_item
+			if line_items.count.zero?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Depot::Application.routes.draw do
+
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
