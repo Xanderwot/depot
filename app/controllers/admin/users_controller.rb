e61485060e98bf31a8b484fcac5ae5793eaaa4bf
Oleg Haidul <oleghaidul@gmail.com>
2012-04-04 20:46:42 +0300

    fix updating user roles in admin namespace

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,27 +1,29 @@
 class Admin::UsersController < AdminController
+  before_filter :authenticate_user!
 
-    load_and_authorize_resource
+  load_and_authorize_resource
 
-	def create
-        @user = User.new(params[:user])
-        if @user.save
-            redirect_to admin_users_path
-        else
-            render :new
-        end  	 
-	end
 
-	def update
-        if @user.update_attributes(params[:user])
-       	    redirect_to admin_users_path
-        else
-            render :new
-        end
-	end	
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end      
+  end
 
-	def destroy
-        @user.destroy
-        redirect_to admin_users_path
-	end	
+  def update
+    if @user.update_attributes(params[:user])
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end 
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+  end 
 
 end
